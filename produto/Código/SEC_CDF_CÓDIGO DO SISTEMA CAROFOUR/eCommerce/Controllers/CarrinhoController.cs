ed627ef0f307ac245a677809abd664a787d90739
deborahhelen <deborahhelen.s@gmail.com>
2014-04-02 19:28:34 -0300

    Código Fonte
    Commit da primeira versão desenvolvida.

---
+++ produto/Código/SEC_CDF_CÓDIGO DO SISTEMA CAROFOUR/eCommerce/Controllers/CarrinhoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        //
+        // GET: /Carrinho/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FecharCompra()
+        {
+            return View();
+        }
+
+        public ActionResult DadosPedido()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ produto/Código/SEC_CDF_CÓDIGO DO SISTEMA CAROFOUR/eCommerce/Controllers/ListaItensController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ListaItensController : Controller
+    {
+        //
+        // GET: /ListaItens/
+
+        public ActionResult Index()
+        {
+            return View();
+        }    
+    }
+}
