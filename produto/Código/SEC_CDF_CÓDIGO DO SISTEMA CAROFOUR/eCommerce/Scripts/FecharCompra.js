6e65b66797470f9e975e057b44d3fc382d014e8d
lucasferabreu <lucasfaeeop@gmail..com>
2014-04-08 12:55:34 -0300

    Codigo Fonte
    Atualização do código proveniente da reunião do dia 08/04

---
+++ produto/Código/SEC_CDF_CÓDIGO DO SISTEMA CAROFOUR/eCommerce/Scripts/FecharCompra.js
@@ -0,0 +1,18 @@
+﻿function validarFormularioCompra() {
+   
+    var strMsg = '';
+    if (document.getElementById('txtNome').value == "")
+        strMsg += 'Informe o nome\n';
+    if (document.getElementById('txtDataNascimento').value == "")
+        strMsg += 'Informe a data nascimento\n';  
+
+    if (strMsg != "")
+        alert(strMsg);
+    else
+        alert('Pedido enviado com sucesso');
+
+    return false;
+}
+
+
+
