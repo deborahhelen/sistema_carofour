e3c612e9373ba8732b79a398d106b1f81f8c4dd8
moane prates <moane@gmail.com>
2014-04-08 15:44:16 -0300

    JavaScript, CSS, Internacionalização
    Terminei a validação do  formulario de compra em javaScript, Criei
    algumas classes CSS, coloquei os links para mudar de português para
    espanhol e vice versa

--- produto/Código/SEC_CDF_CÓDIGO DO SISTEMA CAROFOUR/eCommerce/Scripts/FecharCompra.js
+++ produto/Código/SEC_CDF_CÓDIGO DO SISTEMA CAROFOUR/eCommerce/Scripts/FecharCompra.js
@@ -4,8 +4,13 @@
     if (document.getElementById('txtNome').value == "")
         strMsg += 'Informe o nome\n';
     if (document.getElementById('txtDataNascimento').value == "")
-        strMsg += 'Informe a data nascimento\n';  
-
+        strMsg += 'Informe a data nascimento\n';
+    if (document.getElementById('txtEmail').value == "")
+        strMsg += 'Informe o email\n';
+    if (document.getElementById('txtEndereco').value == "")
+        strMsg += 'Informe o endereço\n';
+    if (document.getElementById('txtTelefone').value == "")
+        strMsg += 'Informe o telefone de contato\n';
     if (strMsg != "")
         alert(strMsg);
     else
